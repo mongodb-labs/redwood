e0b67988f54e6bb0b9361052eceb63e4bdd3f785
Nima Taheri <nima.taheri@mongodb.com>
2025-10-19 21:18:07 -0700

    feat: add mongo-cdn presets to options dialog feat: use checkbox for compressed and wds checkbox

--- options.js
+++ options.js
@@ -1,28 +1,72 @@
+setInputs = (items) => {
+  document.querySelector('input[name=match]').value = items.match;
+  document.querySelector('input[name=replace]').value = items.replace;
+  document.querySelector('input[name=wds]').checked = items.wds;
+  document.querySelector('input[name=webServerPort]').value = items.webServerPort;
+  document.querySelector('input[name=compressed]').checked = items.compressed;
+};
+
+setPreset = (event) => {
+  const presets = {
+    Development: {
+      match: 'https://assets-dev.mongodb-cdn.com/mms',
+      replace: 'http://localhost:8081',
+      wds: true,
+      webServerPort: 8081,
+      compressed: false,
+    },
+    QA: {
+      match: 'https://assets-qa.mongodb-cdn.com/mms',
+      replace: 'http://localhost:8081',
+      wds: true,
+      webServerPort: 8081,
+      compressed: false,
+    },
+    Production: {
+      match: 'https://assets.mongodb-cdn.com/mms',
+      replace: 'http://localhost:8081',
+      wds: true,
+      webServerPort: 8081,
+      compressed: false,
+    },
+  };
+
+  setInputs(presets[event.target.value]);
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        match: 'https://example.com',
-        replace: 'http://127.0.0.1:80',
-        wds: true,
-        webServerPort: 8080,
-        compressed: false
-    }, items => {
-        document.querySelector('input[name=match]').value = items.match
-        document.querySelector('input[name=replace]').value = items.replace
-        document.querySelector('input[name=wds]').value = items.wds
-        document.querySelector('input[name=webServerPort]').value = items.webServerPort
-        document.querySelector('input[name=compressed]').value = items.compressed
-    })
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get(
+    {
+      match: 'https://example.com',
+      replace: 'http://127.0.0.1:80',
+      wds: true,
+      webServerPort: 8080,
+      compressed: false,
+    },
+    (items) => {
+      setInputs(items);
+    }
+  );
 
-    const getInput = name => document.querySelector(`input[name=${name}]`).value
+  const getInput = (name) => document.querySelector(`input[name=${name}]`).value;
+  const getCheckbox = (name) => document.querySelector(`input[name=${name}]`).checked;
 
-    document.querySelector('button[name=save]').addEventListener('click', () => {
-        chrome.storage.sync.set({
-            match: getInput('match'),
-            replace: getInput('replace'),
-            wds: getInput('wds'),
-            webServerPort: getInput('webServerPort'),
-            compressed: getInput('compressed')
-        }, chrome.runtime.reload)
-    })
-})
+  document.querySelectorAll('input[name=preset]').forEach((button) => {
+    button.addEventListener('click', setPreset);
+  });
+
+  document.querySelector('button[name=save]').addEventListener('click', () => {
+    chrome.storage.sync.set(
+      {
+        match: getInput('match'),
+        replace: getInput('replace'),
+        wds: getCheckbox('wds'),
+        webServerPort: getInput('webServerPort'),
+        compressed: getCheckbox('compressed'),
+      },
+      chrome.runtime.reload
+    );
+    window.close();
+  });
+});
